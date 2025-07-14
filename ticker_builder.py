6cfd2c6166a3f4a64772fd25cdd84cff1dd7a2f0
Keelan Byrne <kbyrne@interactivebrokers.co.uk>
2025-07-14 10:09:41 +0100

    update future contract requirements and some typos

--- ticker_builder.py
+++ ticker_builder.py
@@ -17,6 +17,11 @@ class TickerBuilder:
         "tradingClass" : "tc=",
     }
 
+    # Build dictionary containing required ticket fields for each security type
+    requirements = dict.fromkeys(["STK", "FUND", "IND", "BOND", "CFD", "CMDTY", "CASH", "CRYPTO"], ["symbol", "secType", "exchange", "currency"])
+    requirements.update(dict.fromkeys(["FUT"], requirements["STK"] + ["multiplier", "lastTradeDateOrContractMonth"]))
+    requirements.update(dict.fromkeys(["OPT", "FOP", "WAR"], requirements["FUT"] + ["strike", "right"]))
+
     def __init__(self, contract):
         self.security_type = contract.secType
         self.contract = contract
@@ -25,14 +30,9 @@ class TickerBuilder:
     def build_ticker(self):
         """Function to build ticker based on secType requirements"""
         ticker = []
-        
-        # Build dictionary containing requried ticket fields for each secutiry type
-        requirements = dict.fromkeys(["STK", "FUND", "IND", "BOND", "CFD", "CMDTY", "CASH"], ["symbol", "secType", "exchange", "currency"])
-        requirements.update(dict.fromkeys(["FUT"], requirements["STK"] + ["localSymbol", "lastTradeDateOrContractMonth"]))
-        requirements.update(dict.fromkeys(["OPT", "FOP", "WAR"], requirements["FUT"] + ["multiplier", "strike", "right"]))
 
         try:
-            for ticker_field in requirements[self.security_type]:
+            for ticker_field in self.requirements[self.security_type]:
                 ticker.append(f'"{self.ticker_fields[ticker_field]}{getattr(self.contract, ticker_field)}"')
         except KeyError:
             return False
